0975b5a4e03d3e36793e842f1df36c453b18d870
poeticalbyte <poeticalbyte@gmail.com>
2026-06-04 03:41:43 -0500

    fix prisma client generation

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,10 @@
-import { PrismaClient } from './generated/prisma/client'
+import { PrismaClient } from '@prisma/client'
 
 declare global {
   var prisma: PrismaClient | undefined
 }
 
-export const prisma =
-  globalThis.prisma ?? new PrismaClient({} as any)
+export const prisma = globalThis.prisma ?? new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') {
   globalThis.prisma = prisma
