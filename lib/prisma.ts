11e5912217e6346d030a8f095fbeca023091cf27
poeticalbyte <poeticalbyte@gmail.com>
2026-06-08 22:47:17 -0500

    fix (second): changed prisma stuff

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,16 @@
 import { PrismaClient } from '@prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
 
 declare global {
   var prisma: PrismaClient | undefined
 }
 
-export const prisma = globalThis.prisma ?? new PrismaClient()
+const connectionString = process.env.DATABASE_URL ?? ''
+
+export const prisma =
+  globalThis.prisma ?? new PrismaClient({
+    adapter: new PrismaPg({ connectionString }),
+  })
 
 if (process.env.NODE_ENV !== 'production') {
   globalThis.prisma = prisma
